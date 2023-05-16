0e004f2719281768e5ace0c8c0c57d5edf72b038
Ofek Buchnik <ofekyvb@gmail.com>
2023-05-16 18:54:49 +0300

    first commit

---
+++ src/main/java/gemesil/fasterpathways/FasterPathways.java
@@ -0,0 +1,17 @@
+package gemesil.fasterpathways;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FasterPathways extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
